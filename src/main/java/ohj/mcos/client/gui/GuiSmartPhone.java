369b605ff51d5b26ce49243de26cb98ab40afcab
OldHuaJi <lzkdfm@foxmail.com>
2018-05-20 21:57:23 +0800

    May/20/2018-LU-DU

--- src/main/java/ohj/mcos/client/gui/GuiSmartPhone.java
+++ src/main/java/ohj/mcos/client/gui/GuiSmartPhone.java
@@ -1,7 +1,9 @@
 package ohj.mcos.client.gui;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
@@ -15,7 +17,10 @@ import ohj.mcos.MainmcOS;
 @SideOnly(Side.CLIENT)
 public class GuiSmartPhone extends GuiScreen {
     private ResourceLocation texture1 = new ResourceLocation(MainmcOS.MODID, "textures/gui/guismartphone.png");
-    public final int xSize = 176, ySize = 256, bottonHome = 0, buttonBack = 1, buttonMananer = 2, buttonSeu = 3, buttonDangerous = 4, buttonAbout = 5, buttonDriveMananer = 6, buttonAR = 7, buttonAnnex = 8;
+    public final int xSize = 176, ySize = 256, maxPoint = 8;
+    int var1 = 0;
+
+    //    bottonHome = 0, buttonBack = 1, buttonMananer = 2, buttonSeu = 3, buttonDanger = 4, buttonAbout = 5, buttonDriveMananer = 6, buttonAR = 7, buttonAnnex = 8,
 
     public GuiSmartPhone() {
     }
@@ -26,31 +31,48 @@ public class GuiSmartPhone extends GuiScreen {
         int offsetX = (this.width - this.xSize) / 2,
                 offsetY = (this.height - this.ySize) / 2;
         this.buttonList.clear();
-        this.buttonList.add(new GuiButton(bottonHome, offsetX + 153, offsetY + 17, 15, 10, I18n.format("gui.smartphone.button.0")));
-        this.buttonList.add(new GuiButton(buttonBack, offsetX + 153, offsetY + 16, 15, 10, I18n.format("gui.smartphone.button.1")));
-        this.buttonList.add(new GuiButton(buttonMananer, offsetX + 153, offsetY + 15, 15, 10, I18n.format("gui.smartphone.button.2")));
-        this.buttonList.add(new GuiButton(buttonSeu, offsetX + 153, offsetY + 14, 15, 10, I18n.format("gui.smartphone.button.3")));
-        this.buttonList.add(new GuiButton(buttonDangerous, offsetX + 153, offsetY + 13, 15, 10, I18n.format("gui.smartphone.button.4")));
-        this.buttonList.add(new GuiButton(buttonAbout, offsetX + 153, offsetY + 12, 15, 10, I18n.format("gui.smartphone.button.5")));
-        this.buttonList.add(new GuiButton(buttonDriveMananer, offsetX + 153, offsetY + 11, 15, 10, I18n.format("gui.smartphone.button.6")));
-        this.buttonList.add(new GuiButton(buttonAR, offsetX + 153, offsetY + 10, 15, 10, I18n.format("gui.smartphone.button.7")));
-        this.buttonList.add(new GuiButton(buttonAnnex, offsetX + 153, offsetY + 9, 15, 10, I18n.format("gui.smartphone.button.8")));
+        while (var1 <= maxPoint) {
+            this.buttonList.add(new GuiButton(var1, offsetX + 153, offsetY + 17 - var1, 15, 10, I18n.format("gui.smartphone.button." + var1)) {
+                @Override
+                public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks) {
+                    if (this.visible) {
+                        GlStateManager.color(1.0F, 1.0F, 1.0F);
+
+                        mc.getTextureManager().bindTexture(TEXTURE);
+                        int x = mouseX - this.xPosition, y = mouseY - this.yPosition;
+
+                        if (x >= 0 && y >= 0 && x < this.width && y < this.height) {
+                            this.drawTexturedModalRect(this.xPosition, this.yPosition, 1, 146, this.width, this.height);
+                        } else {
+                            this.drawTexturedModalRect(this.xPosition, this.yPosition, 1, 134, this.width, this.height);
+                        }
+                    }
+                }
+            }
+        }
+        );
+        var1++;
+        if (var1 >= maxPoint + 1) {
+            var1 = 0;
+        }
+
 
     }
 
     @Override
     public void drawScreen(int x, int y, float ticks) {
-        int var1 = 0;
+        this.mc.getTextureManager().bindTexture(texture1);
+
         int offsetX = (this.width - this.xSize) / 2, offsetY = (this.height - this.ySize) / 2;
         this.drawTexturedModalRect(offsetX, offsetY, 0, 0, this.xSize, this.ySize);
         super.drawScreen(x, y, ticks);
-        while (var1 <= 8) {
+        /*while (var1 <= 8) {
 
-            if (buttonList.get(var1).visible) {
-                this.mc.getTextureManager().bindTexture(texture1);
+            if (buttonList.get(var1).visible && var1<=8) {
                 var1++;
-            }
-        }
+            }else{
+            var1=0}
+        }*/
     }
 
     @Override
